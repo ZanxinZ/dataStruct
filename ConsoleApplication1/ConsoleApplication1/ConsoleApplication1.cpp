7fbbdac1c2407b949a86c1142a390801f4ad4094
ZanxinZ <1252622709@qq.com>
2020-04-08 12:24:25 +0800

    update second

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,13 +1,14 @@
 // ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
 //
 
-
+#include "stdafx.h"
 #include <iostream>
 using namespace std;
 
 int main()
 {
 	cout << "welcom" << endl;
+	cout << "again" << endl;
     return 0;
 }
 
